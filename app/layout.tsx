bdf050a1973f76e221c1990c2c33fef767e9e8eb
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-09-19 20:42:45 +0300

    fINISh

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,7 +121,7 @@ export default async function RootLayout({
           id="snap-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};a.queue=[];var s='script';r=t.createElement(s);r.async=!0;r.src=n;var u=t.getElementsByTagName(s)[0];u.parentNode.insertBefore(r,u);})(window,document,'https://sc-static.net/scevent.min.js');snaptr('init','3c5a4326-d616-4185-97fa-17ff702018f6',{});snaptr('track','PAGE_VIEW');`,
+            __html: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};a.queue=[];var s='script',r=t.createElement(s);r.async=!0;r.src=n;var u=t.getElementsByTagName(s)[0];u.parentNode.insertBefore(r,u);})(window,document,'https://sc-static.net/scevent.min.js');snaptr('init','3c5a4326-d616-4185-97fa-17ff702018f6',{});snaptr('track','PAGE_VIEW');`,
           }}
         />
       </body>
