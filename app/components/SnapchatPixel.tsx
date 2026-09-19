b58b54ac4c050782b9ad122e4c5c2f9aa9232066
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-09-19 21:12:55 +0300

    fINISh

--- app/components/SnapchatPixel.tsx
+++ app/components/SnapchatPixel.tsx
@@ -8,7 +8,7 @@ export default function SnapchatPixel() {
       id="snapchat-pixel"
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
-        __html: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};a.queue=[];var s='script',r=t.createElement(s);r.async=!0;r.src=n;var u=t.getElementsByTagName(s)[0];u.parentNode.insertBefore(r,u);})(window,document,'https://sc-static.net/scevent.min.js');snaptr('init','3c5a4326-d616-4185-97fa-17ff702018f6',{});snaptr('track','PAGE_VIEW');`,
+        __html: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};a.queue=[];var s='script',r=t.createElement(s);r.async=!0;r.src=n;var u=t.getElementsByTagName(s)[0];u.parentNode.insertBefore(r,u);})(window,document,'https://sc-static.net/scevent.min.js');snaptr('init','3c5a4326-d616-4185-97fa-17ff702018f6',{});snaptr('init','70a03153-424c-47f0-a28e-aac9c6e02227',{});snaptr('track','PAGE_VIEW');`,
       }}
     />
   );
