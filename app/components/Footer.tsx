65d0a0fddf210c9926ef01f6c6800eb4b7019c80
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-09-21 14:05:13 +0300

    F

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -67,7 +67,7 @@ export default async function Footer() {
                   <span className="text-xs text-gray-500 font-mono">7055249325</span>
                 </div>
               </a>
-              <a href="https://eauthenticate.saudibusiness.gov.sa/inquiry" target="_blank" rel="noreferrer"
+             {/* <a href="https://eauthenticate.saudibusiness.gov.sa/inquiry" target="_blank" rel="noreferrer"
                 className="flex items-center gap-2 group">
                 <Image src="/work.webp" alt="شهادة توثيق" width={36} height={36} className="object-contain rounded-md shrink-0" />
                 <div className="flex flex-col">
@@ -75,7 +75,7 @@ export default async function Footer() {
                   <span className="text-xs text-gray-500">مركز الاعمال</span>
                   <span className="text-xs text-gray-500 font-mono">0000325383</span>
                 </div>
-              </a>
+              </a> */}
             </div>
           </div>
 
